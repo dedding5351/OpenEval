e98ae8725b0bad710fe25b7d6c5e86e67f74d114
Erika Trejo <etrejo@lawn-143-215-114-106.lawn.gatech.edu>
2018-09-28 10:54:49 -0400

    create default survey endpoint

--- Server/api/routes/surveys.js
+++ Server/api/routes/surveys.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const Survey = require('../models/survey');
+const Default = require('../models/questions');
+
 
 
 router.get('/:professor',(req, res, next) => {
@@ -25,6 +27,35 @@ router.get('/:professor/:course',(req, res, next) => {
         });
     });
 
+});
+
+router.post('/default/:startTime/:endTime/:surveyName/:course/:semester/:professor', (req, res, next) => {
+    const arr = Default.find({'type': 'default'}).exec(function(err,result) {
+        if (err) throw err;
+        const q = result[0].questions;
+        var questions = [];
+        for (var i = 0; i < q.length; i++) {
+            questions.push(q[i].question);
+        }
+        const survey = new Survey({
+            _id: new mongoose.Types.ObjectId(),
+            questions: questions,
+            startTime: req.params.startTime,
+            endTime: req.params.endTime,
+            name: req.params.surveyName,
+            course: req.params.course,
+            semester: req.params.semester,
+            professor: req.params.professor
+        });
+        survey.save().then(result => {
+            console.log(result);
+        })
+        .catch(err => console.log(err));
+        res.status(200).json({
+            message: 'Handling POST requests to /surveys',
+            createdCourse: survey
+        });
+    });
 });
 
 
